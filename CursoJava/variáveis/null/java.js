f5ece0e3484164a2127b7ecc17d82ff9c244f2ea
Júlia Garcia <spacejugarcia@gmail.com>
2023-05-24 21:16:40 -0300

    .

--- CursoJava/variáveis/null/java.js
+++ CursoJava/variáveis/null/java.js
@@ -1,3 +1,11 @@
+//hoisting - içamento //
+
 var nome = null;
 
-console.log(null) = "matheus"
+console.log(null);
+nome = "matheus"
+
+console.log(nome); 
+console.log (sobrenome);
+
+// as funções e variasveis sejam inicializadas no começo do código//
---
+++ CursoJava/variáveis/objetos/java2a.js
@@ -0,0 +1,9 @@
+var obj = {
+    nome: "Matheus",
+    idade: 29,
+    profissão: "programador",
+}
+
+console.log(obj);
+
+console.log("meu nome é"+ obj.nome)
