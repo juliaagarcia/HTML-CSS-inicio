7d116eb263a2df3feb8a568b10dd4533615a358c
Júlia Garcia <spacejugarcia@gmail.com>
2023-07-06 19:25:14 -0300

    .

--- CursoJava/variáveis/if/java2a.js
+++ CursoJava/variáveis/if/java2a.js
@@ -1,3 +1,16 @@
 if(false) {
     console.log("testando if")
 }
+
+console.log("depois do if")
+
+var idade = 19 
+
+if (idade > 18) {
+console.log("pode fazer carteira de habilitação ")
+
+}
+
+if (idade > 15) {
+    console.log("precisa esperar mais 3anos")
+}
