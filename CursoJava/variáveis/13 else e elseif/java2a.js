29752e91b7522564d56b38d0328259395c2bba36
Júlia Garcia <spacejugarcia@gmail.com>
2023-07-16 19:10:36 -0300

    .

---
+++ CursoJava/variáveis/13 else e elseif/java2a.js
@@ -0,0 +1,9 @@
+var name = "Matheus";
+
+if(nome =="pedro") {
+    console.log("O nome dele é Pedro");
+} else if(nome = "matheus") {
+    console.log("O nome é Matheus")
+}else {
+    console.log("ele possui outro nome")
+}
