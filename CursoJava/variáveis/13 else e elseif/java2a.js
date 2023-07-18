d29252a2b1074080697d45357c091d66c1dcdae9
Júlia Garcia <spacejugarcia@gmail.com>
2023-07-18 12:26:55 -0300

    exercícios

--- CursoJava/variáveis/13 else e elseif/java2a.js
+++ CursoJava/variáveis/13 else e elseif/java2a.js
@@ -1,9 +1,11 @@
-var name = "Matheus";
+var name = "Xavier";
 
 if(nome =="pedro") {
     console.log("O nome dele é Pedro");
 } else if(nome = "matheus") {
-    console.log("O nome é Matheus")
-}else {
+    console.log("O nome é Matheus");
+    }else if(nome == "Xavier" ){
+console.log("O nome é Xavier")
+    } else {
     console.log("ele possui outro nome")
 }
---
+++ manipulação de array/manipulção.js
@@ -0,0 +1,5 @@
+//push//
+
+frutas=['laranja', 'abacaxi', 'pera']
+frutas.push(limao)
+console.log(frutas);
