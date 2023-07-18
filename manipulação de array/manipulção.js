0d6a51bee443ab270104ee8821095cb398d06619
Júlia Garcia <spacejugarcia@gmail.com>
2023-07-18 12:50:12 -0300

    .

--- manipulação de array/manipulção.js
+++ manipulação de array/manipulção.js
@@ -1,5 +1,39 @@
 //push//
 
-frutas=['laranja', 'abacaxi', 'pera']
-frutas.push(limao)
-console.log(frutas);
+ let navegadores = [ 'chrome','firefox', 'edge']
+
+ navegadores.push('opera','microsofth')
+
+ console.log(navegadores);
+
+ //pop//
+
+let frutas = ['pera','limão','laranja']
+ frutas.pop()
+ console.log(frutas);
+
+//unshit//
+
+let nome = ['Jessica', 'Elisangela']
+nome.unshift('Fabiana')
+console.log(nome);
+
+//shift//
+
+let carros = ['bmw','fusca','gol bolinha']
+carros.shift()
+console.log(carros);
+
+//length//
+
+let cores = ['vermelho','verde','azul']
+console.log(cores.length)
+
+//index//
+
+let sobrenomes = ['silva','santos','pereira']
+sobrenomes.indexOf('pereira')
+
+
+
+
